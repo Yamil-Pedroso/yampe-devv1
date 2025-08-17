af47835df0a0a954fbd3280f6a1e3547a1d627c2
Yamil Pedroso <yamirovinci@gmail.com>
2025-08-17 02:49:01 +0200

    Destructuration data get about me aboutService

--- portfolio-web/src/services/AboutService.ts
+++ portfolio-web/src/services/AboutService.ts
@@ -1,19 +1,7 @@
 import apiClient from "../api/axiosConfig";
 import type { CommonContent } from "@/types/Types";
 
-type AboutApiResponse = {
-  status: "success" | "error";
-  header: string;
-  description: string;
-  title?: string;
-};
-
 export const getAboutMe = async (): Promise<CommonContent> => {
-  const { data } = await apiClient.get<AboutApiResponse>("/about");
-  // mapear a tu DTO de frontend
-  return {
-    header: data.header,
-    title: data.title,
-    description: data.description,
-  };
+  const { data } = await apiClient.get<CommonContent>("/about");
+  return data;
 };
