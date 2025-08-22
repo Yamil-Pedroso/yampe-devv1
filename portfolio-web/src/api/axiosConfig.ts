6b47aa515b7907dbaebd92bf450b48a6405bff1b
root <root@srv838088.hstgr.cloud>
2025-08-22 16:55:59 +0000

    Update portfolio

--- portfolio-web/src/api/axiosConfig.ts
+++ portfolio-web/src/api/axiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:3010/api";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 const apiClient = axios.create({
   baseURL,
