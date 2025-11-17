0f13c693380bba308d3a3a1bc721029932974da7
Yamil Pedroso <yamirovinci@gmail.com>
2025-11-17 18:42:30 +0100

    Optimize image into webp

--- portfolio-web/src/assets/index.ts
+++ portfolio-web/src/assets/index.ts
@@ -2,11 +2,9 @@
 import code from "./pixel-icons/svg/code.svg";
 
 // Videos
-import templatesWebsiteVideo from "./video/templates_website.mp4";
 import video1 from "./video/video1.mp4";
 
 const assets = {
-  templatesWebsiteVideo,
   video1,
   // SVG Icons
   code,
