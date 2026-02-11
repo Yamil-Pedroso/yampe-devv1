840e9902d5a81222e0ab9d6a75d7028fafc4cd8d
Yamil Pedroso <yamirovinci@gmail.com>
2026-02-11 17:28:09 +0100

    Add a new url to cors

--- portfolio-web/src/api/axiosConfig.ts
+++ portfolio-web/src/api/axiosConfig.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_BASE_URL || "/api";
+const baseURL =
+  import.meta.env.VITE_API_BASE_URL || import.meta.env.API_BASE_URL || "/api";
 
 const apiClient = axios.create({
   baseURL,
